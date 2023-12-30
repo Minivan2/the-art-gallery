3e2794093d7937b08adc1a7043e812ed8374578c
Van Khoa Tran <vanxz489@proton.me>
2023-12-30 17:08:40 -0600

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <main> 
+      <header>
+        <div className="topbar">
+          <a href="" class="logo">The Blog</a>
+        </div>
+        <nav>
+          <a href="" className="login">Login</a>
+          <a href="" className="register">Register</a>
+        </nav>
+      </header>
+    </main>
+  );
+}
+
+export default App;
