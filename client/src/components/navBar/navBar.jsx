cbb377a832dcfc0a0712a2e00ce50d3c87977c84
Van Khoa Tran <vanxz489@proton.me>
2023-12-30 18:46:29 -0600

    Add padding to left and right div of NavBar

--- client/src/components/navBar/navBar.jsx
+++ client/src/components/navBar/navBar.jsx
@@ -3,16 +3,16 @@ import './navBar.css';
 export default function NavBar() {
     return (
         <nav>
-          <div>
+          <div className='navLeft'>
             <a href="" class="logo">The Art Museum</a>
           </div>
-          <div className="center">
+          <div className="navCenter">
             <a href="#home">Home</a>
             <a href="#about">About</a>
             <a href="#showcase">Showcase</a>
             <a href="#contact">Contact</a>
           </div>
-          <div className="right">
+          <div className="navRight">
             <span class="material-symbols-outlined">account_circle</span>
           </div>
         </nav>
