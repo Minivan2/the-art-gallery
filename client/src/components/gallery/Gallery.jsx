90dd95067c4b508ccde4b98e11ecbf68c0476f37
Van Khoa Tran <vanxz489@proton.me>
2024-01-01 15:41:49 -0600

    Remove unneeded dependencies

--- client/src/components/gallery/Gallery.jsx
+++ client/src/components/gallery/Gallery.jsx
@@ -8,9 +8,9 @@ export default function Home() {
                 <img src="https://artincontext.org/wp-content/uploads/2021/10/Asian-Artwork.avif" alt="A landscape painting depicting a valley"/>
             </div>
             <div className="text">
-                <h3 class='title'>Moonlight Landscape (1889)</h3>
-                <h5 class='author'>by Hashimoto Gahō</h5>
-                <p class='preview'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed felis eget velit aliquet sagittis id consectetur purus ut.</p>
+                <h3 className='title'>Moonlight Landscape (1889)</h3>
+                <h5 className='author'>by Hashimoto Gahō</h5>
+                <p className='preview'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed felis eget velit aliquet sagittis id consectetur purus ut.</p>
             </div>
         </div>
         <div class='card'>
@@ -18,9 +18,9 @@ export default function Home() {
                 <img src="https://www.worldhistory.org/uploads/images/15464.png?v=1647534147" alt="A painting depicting the starry night"/>
             </div>
             <div className="text">
-                <h3>The Starry Night (1889)</h3>
-                <h5>by Vincent van Gogh</h5>
-                <p>Van Gogh's night sky is a field of roiling energy. Below the exploding stars, the village is a place of quiet order. Connecting earth and sky is the flamelike cypress, a tree traditionally associated with graveyards and mourning.</p>
+                <h3 className='title'>The Starry Night (1889)</h3>
+                <h5 className='author'>by Vincent van Gogh</h5>
+                <p className='preview'>Van Gogh's night sky is a field of roiling energy. Below the exploding stars, the village is a place of quiet order. Connecting earth and sky is the flamelike cypress, a tree traditionally associated with graveyards and mourning.</p>
             </div>
         </div>
         <div class='card'>
@@ -28,9 +28,9 @@ export default function Home() {
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Albrecht_D%C3%BCrer_-_Hare%2C_1502_-_Google_Art_Project.jpg/1280px-Albrecht_D%C3%BCrer_-_Hare%2C_1502_-_Google_Art_Project.jpg" alt="A painting depicting a rabbit"/>
             </div>
             <div className="text">
-                <h3>The Starry Night (1502)</h3>
-                <h5>by Albrecht Dürer</h5>
-                <p>The work at the very heart of the Albertina, Dürer’s famous 'Hase', can only be shown every few years. Duke Albert of Saxe-Teschen received it in 1796 as part of an exchange of artworks with Emperor Francis II.</p>
+                <h3 className='title'>The Starry Night (1502)</h3>
+                <h5 className='author'>by Albrecht Dürer</h5>
+                <p className='preview'>The work at the very heart of the Albertina, Dürer’s famous 'Hase', can only be shown every few years. Duke Albert of Saxe-Teschen received it in 1796 as part of an exchange of artworks with Emperor Francis II.</p>
             </div>
         </div>
         <div class='card'>
@@ -38,9 +38,9 @@ export default function Home() {
                 <img src="https://www.worldhistory.org/uploads/images/15464.png?v=1647534147" alt="A painting depicting the starry night"/>
             </div>
             <div className="text">
-                <h3>The Starry Night (1889)</h3>
-                <h5>by Vincent van Gogh</h5>
-                <p>Van Gogh's night sky is a field of roiling energy. Below the exploding stars, the village is a place of quiet order. Connecting earth and sky is the flamelike cypress, a tree traditionally associated with graveyards and mourning.</p>
+                <h3 className='title'>The Starry Night (1889)</h3>
+                <h5 className='author'>by Vincent van Gogh</h5>
+                <p className='preview'>Van Gogh's night sky is a field of roiling energy. Below the exploding stars, the village is a place of quiet order. Connecting earth and sky is the flamelike cypress, a tree traditionally associated with graveyards and mourning.</p>
             </div>
         </div>
         <div class='card'>
@@ -48,9 +48,9 @@ export default function Home() {
                 <img src="https://www.worldhistory.org/uploads/images/15464.png?v=1647534147" alt="A painting depicting the starry night"/>
             </div>
             <div className="text">
-                <h3>The Starry Night (1889)</h3>
-                <h5>by Vincent van Gogh</h5>
-                <p>Van Gogh's night sky is a field of roiling energy. Below the exploding stars, the village is a place of quiet order. Connecting earth and sky is the flamelike cypress, a tree traditionally associated with graveyards and mourning.</p>
+                <h3 className='title'>The Starry Night (1889)</h3>
+                <h5 className='author'>by Vincent van Gogh</h5>
+                <p className='preview'>Van Gogh's night sky is a field of roiling energy. Below the exploding stars, the village is a place of quiet order. Connecting earth and sky is the flamelike cypress, a tree traditionally associated with graveyards and mourning.</p>
             </div>
         </div>
         <div class='card'>
@@ -58,9 +58,9 @@ export default function Home() {
                 <img src="https://www.worldhistory.org/uploads/images/15464.png?v=1647534147" alt="A painting depicting the starry night"/>
             </div>
             <div className="text">
-                <h3>The Starry Night (1889)</h3>
-                <h5>by Vincent van Gogh</h5>
-                <p>Van Gogh's night sky is a field of roiling energy. Below the exploding stars, the village is a place of quiet order. Connecting earth and sky is the flamelike cypress, a tree traditionally associated with graveyards and mourning.</p>
+                <h3 className='title'>The Starry Night (1889)</h3>
+                <h5 className='author'>by Vincent van Gogh</h5>
+                <p className='preview'>Van Gogh's night sky is a field of roiling energy. Below the exploding stars, the village is a place of quiet order. Connecting earth and sky is the flamelike cypress, a tree traditionally associated with graveyards and mourning.</p>
             </div>
         </div>
     </div>
