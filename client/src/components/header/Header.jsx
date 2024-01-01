6ab5da020919b292d180436091facc83a58b44ab
Van Khoa Tran <vanxz489@proton.me>
2024-01-01 15:24:31 -0600

    Add responsiveness for large screen sizes along with header image

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,9 +1,12 @@
-import './header.css'
+import './header.css';
+import imageSrc from '../../assets/images/header-bg.jpg';
 
 export default function Header() {
   return (
-    <div className='header'>
-      
+    <div className='header-container'>
+      <div className="headerImg">
+        <img src={imageSrc} alt="Header background" className='header-bg'/>
+      </div>
     </div>
   )
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Gallery from '../../components/gallery/Gallery'
+import Header from '../../components/header/Header'
 import './home.css'
 
 export default function Home() {
   return (
     <>
       <div className="home">
+        <Header />
         <Gallery />
       </div>
     </>
